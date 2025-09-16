fa0e2b146cb105333f9f7433c58a269c1d949a45
lucaolv <111596218+lucaolv@users.noreply.github.com>
2025-09-16 15:51:26 -0300

    feat: implement full crud for categories

--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -7,4 +7,8 @@ const router = Router();
 router.post("/", categoryController.createNewCategory);
 router.get("/", categoryController.listCategories);
 
+router.get("/:id", categoryController.getCategoryDetails);
+router.put("/:id", categoryController.updateExistingCategory);
+router.delete("/:id", categoryController.deleteExistingCategory);
+
 export default router;
