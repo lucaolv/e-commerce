f2d90874bff8fc18cac56ec8fe9dafdefdf9b8d6
lucaolv <111596218+lucaolv@users.noreply.github.com>
2025-09-16 14:53:37 -0300

    feat: implement full crud for products

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,22 +1,39 @@
-// src/services/product.service.ts
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Product } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-// Serviço para listar todos os produtos
 export const getAllProducts = async () => {
   return prisma.product.findMany({
-    // Inclui a categoria relacionada a cada produto no resultado
     include: {
       category: true,
     },
   });
 };
 
-// Serviço para criar um novo produto
-// 'data' será o corpo da requisição (req.body) que virá do controller
 export const createProduct = async (data: any) => {
   return prisma.product.create({
     data,
+  });
+};
+
+export const getProductById = async (id: string) => {
+  return prisma.product.findUnique({
+    where: { id },
+    include: {
+      category: true,
+    },
+  });
+};
+
+export const updateProduct = async (id: string, data: Partial<Product>) => {
+  return prisma.product.update({
+    where: { id },
+    data,
+  });
+};
+
+export const deleteProduct = async (id: string) => {
+  return prisma.product.delete({
+    where: { id },
   });
 };
